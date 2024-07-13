842a6a003a759fcd561ad78ec510b96da22b2bc6
Gustavo <gurox15@gmail.com>
2024-07-12 22:14:36 -0500

    add adminlte3

--- copa_america/settings.py
+++ copa_america/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'album',
+    'adminlte3',
+    'adminlte3_theme',
 ]
 
 MIDDLEWARE = [
